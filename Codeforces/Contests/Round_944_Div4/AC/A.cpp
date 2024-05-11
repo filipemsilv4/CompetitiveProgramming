222a5eab6e0328cbce050a05d48fa8118e15841c
Paulo Filipe Moreira da Silva <59426206+filipemsilv4@users.noreply.github.com>
2024-05-11 09:21:19 -0300

    Create A.cpp

---
+++ Codeforces/Contests/Round_944_Div4/AC/A.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    while (t--){
+        int a,b;
+        cin >> a >> b;
+
+        cout << min(a, b) << " " << max(a, b) << endl;
+    }
+}
