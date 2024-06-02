99af193e45c67e484a4cc6a86e66418e0952c454
filipemsilv4 <filipemsilva@outlook.com.br>
2024-06-02 18:13:45 -0300

    Create TEMPLATE.cpp

---
+++ Codeforces/Contests/TEMPLATE.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <map>
+
+#define lli long long int
+#define ulli unsigned long long int
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+
+    }
+}
