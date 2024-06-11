e9fea836421daaaa03b61364dd872d98d2eab20e
filipemsilv4 <filipemsilva@outlook.com.br>
2024-06-11 14:49:58 -0300

    Create B.cpp

---
+++ Codeforces/Contests/Round_952_Div4/AC/B.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <vector>
+
+#define lli long long int
+#define ulli unsigned long long int
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int n;
+    cin >> n;
+
+    if (n == 3){
+        cout << 3 << endl;
+        continue;
+    }
+
+    cout << 2 << endl;
+  }
+}
