26896cab090a2d9af7a6855bf57a97623a227799
filipemsilv4 <filipemsilva@outlook.com.br>
2024-06-11 14:49:51 -0300

    Create A.cpp

---
+++ Codeforces/Contests/Round_952_Div4/AC/A.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <map>
+
+#define lli long long int
+#define ulli unsigned long long int
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        string a, b;
+        cin >> a >> b;
+        swap(a[0], b[0]);
+
+        cout << a << " " << b << endl;
+    }
+}
