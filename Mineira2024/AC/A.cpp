f8eb66c577dfdd40758e04eb510431e971fbe030
filipemsilv4 <filipemsilva@outlook.com.br>
2024-06-17 17:40:46 -0300

    Create A.cpp

---
+++ Mineira2024/AC/A.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <map>
+
+#define lli long long int
+#define ulli unsigned long long int
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    cout << 2024 - ((2024 - n) * 2) << endl
+}
