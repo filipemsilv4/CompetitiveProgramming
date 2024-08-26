554ed4ff92e4d48f1ac1c36daa2211c236b87552
filipemsilv4 <filipemsilva@outlook.com.br>
2024-08-26 13:49:38 -0300

    Create TEMPLATE.cpp

---
+++ TEMPLATE.cpp
@@ -0,0 +1,23 @@
+/*
+g++ -std=c++17 -o bin/out a.cpp && ./bin/out
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <map>
+
+#define lli long long int
+#define ulli unsigned long long int
+
+using namespace std;
+
+int main(){
+    lli t;
+    cin >> t;
+    while (t--){
+
+    }
+}
